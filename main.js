437f90da8dcb0dc3f958d26ea6806ed298e5f19e
123arnav <74090758+123arnav@users.noreply.github.com>
2021-04-11 11:41:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,23 @@ canvas=createCanvas(225,225);
 canvas.position(600,340);
 video=createCapture(VIDEO);
 video.hide();
-
+console.log(ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XehrBLTBr/model.json",modelloaded);
+}
+function modelloaded(){
+    console.log("model");
 }
 function draw(){
 image(video,0,0,225,225);
+classifier.classify(video,gotresult);
+}
+function gotresult(error,result){
+if (error){
+    console.log(error);
+}
+else{
+    console.log(result)
+    document.getElementById("object").innerHTML=result[0].label;
+    document.getElementById("accuracy").innerHTML=result[0].confidence.toFixed(2);
+}
 }
